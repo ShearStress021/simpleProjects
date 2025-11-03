0d18d396db7176fe302ddb2f2eb808656d541061
shearstress021 <shearstress021@gmail.com>
2025-11-03 01:41:07 -0800

    leave request processor

---
+++ java/LeaveRequestProcessor/src/test/java/com/example/leaverequestprocessor/LeaveRequestProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaverequestprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveRequestProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
