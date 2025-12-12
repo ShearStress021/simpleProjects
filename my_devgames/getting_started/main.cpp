7ed91bb24c3115eeab25215d8543b37bd67968b8
shearstress021 <shearstress021@gmail.com>
2025-12-11 20:21:29 -0800

    tested collision

--- my_devgames/getting_started/main.cpp
+++ my_devgames/getting_started/main.cpp
@@ -26,17 +26,18 @@ int main(){;
 
 	int l_r_x {r_x};
 	int r_r_x{r_x + r_l};
-	int u_r_x{r_y};
-	int b_r_x{r_y + r_l};
+	int u_r_y{r_y};
+	int b_r_y{r_y + r_l};
 
 
-	int direction{10};
+	int upDown{10};
+	int leftRight{10};
 
 
-	bool collision{true};
-
-	
-
+	bool collision = (b_r_y >= u_circle_y) && 
+					(u_r_y <= b_circle_y) && 
+					(r_r_x >= l_circle_x) &&
+					(l_r_x <= r_circle_x);
 
 	
 	SetTargetFPS(60);
@@ -44,16 +45,33 @@ int main(){;
 		BeginDrawing();
 		ClearBackground(WHITE);
 
-
 		if (collision){
 			DrawText("Game Over",400,200,25,RED);
 		}else {
 
 			// Game Logic begin
+			//
+			// Update edges
+			l_circle_x = c_x - c_r;
+			r_circle_x = c_x + c_r;
+			u_circle_y = c_y - c_r;
+			b_circle_y = c_y + c_r;
+			
+
+			l_r_x = r_x;
+			r_r_x = r_x + r_l;
+			u_r_y = r_y;
+			b_r_y = r_y + r_l;
 
 			DrawCircle(c_x, c_y,c_r, BLUE);
 			DrawRectangle(r_x, r_y, r_l,r_l,RED);
 
+
+			
+		 	collision = (b_r_y >= u_circle_y) && 
+							(u_r_y <= b_circle_y) && 
+							(r_r_x >= l_circle_x) &&
+							(l_r_x <= r_circle_x);
 
 
 
@@ -62,17 +80,17 @@ int main(){;
 			// Moving
 
 
-			r_y += direction;
+			r_y += upDown;
 			if (r_y > height || r_y < 0){
-				direction  =  -direction;
+				upDown=  -upDown;
 
 			}
-			if (IsKeyDown(KEY_D) && c_x <= width){
-				c_x += direction;
+			if (IsKeyPressed(KEY_D) && c_x <= width){
+				c_x += leftRight;
 			}
 
 			if(IsKeyDown(KEY_A) && c_x >= 0){
-				c_x -= 10;
+				c_x -= leftRight;
 			}
 
 
