c34a8f0c3e6c597a9821cf15b317596f2df16991
sam <shearstress021@gmail.com>
2025-12-22 16:00:20 -0800

    my engine

---
+++ my_devgames/VulcanGlfw/lve_window.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <string>
+
+namespace lve {
+    class LveWindow{
+        public:
+            LveWindow(int w, int h, std::string name);
+            bool shouldClose(){return glfwWindowShouldClose(window);}
+            LveWindow(const LveWindow&) = delete;
+            LveWindow& operator=(const LveWindow&) = delete;
+            ~LveWindow();
+        private:
+            void initWindow();
+            const int width{};
+            const int height{};
+            std::string windowName{};
+            GLFWwindow * window{};
+    };
+}
