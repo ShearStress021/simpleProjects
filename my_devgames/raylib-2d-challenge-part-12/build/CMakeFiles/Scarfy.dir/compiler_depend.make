6f9eef1f8058c622bb804950a7f18e4cf1fb9a15
shearstress021 <shearstress021@gmail.com>
2026-02-13 21:13:01 -0800

    refences

---
+++ my_devgames/raylib-2d-challenge-part-12/build/CMakeFiles/Scarfy.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Scarfy.
+# This may be replaced when dependencies are built.
---
+++ my_devgames/raylib-2d-challenge-part-12/build/CMakeFiles/link_to_data.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/link_to_data"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/link_to_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
