b1556425177cbd46d765e3dd4d9dced6d75dd114
sam <shearstress021@gmail.com>
2026-06-11 20:13:17 -0700

    linked list data structure with smart pointer

---
+++ DataStructures/DataStructures/llist.hpp
@@ -0,0 +1,107 @@
+#pragma once
+#include <memory>
+
+
+template<typename T>
+struct Node {
+
+	T data{};
+    Node* next{};
+};
+
+template<typename T>
+class LinkedList {
+
+public:
+
+	~LinkedList() {
+
+	}
+
+	void insertNodeAtBegin(const T& val) {
+		head = new Node{ val, head };
+	}
+	void insertNodeAtEnd(const T& val) {
+
+		Node<T>* newNode = new Node<T>{ val, nullptr };
+
+		if (!head) {
+			head = newNode;
+			return;
+		}
+
+
+		Node<T>* current = head;
+		while (current->next) {
+			current = current->next;
+
+		}
+		current->next = newNode;
+
+	}
+
+	bool removeAtBegin() {
+
+		if (!head) {
+			return false;
+		}
+		Node<T>* delNode = head;
+		head = head->next;
+		delete delNode;
+	}
+
+	bool removeAtEnd() {
+		if (!head) {
+			return false;
+		}
+
+		if (!head->next) {
+			return removeAtBegin();
+		}
+
+
+		Node<T>* current = head;
+		while (current->next->next)
+			current = current->next;
+
+		delete current->next;
+		current->next = nullptr;
+		return true;
+	}
+
+
+	bool removeByValue(const T& val) {
+		if (!head) {
+			return false;
+		}
+		if (head->data == val) {
+			return removeAtBegin();
+
+		}
+		Node<T>* current = head;
+		while (current->next && current->next->data != val) {
+			current = current->next;
+		}
+
+		if (!current->next) {
+			return false;
+		}
+		Node<T>* delNode = current->next;
+		current->next = delNode->next;
+		delete delNode;
+
+		return true;
+	}
+	void print() {
+		Node<T>* current = head;
+		while (current) {
+			std::cout << current->data << " --> ";
+			current = current->next;
+		}
+		std::cout << "nullptr\n";
+	}
+
+private:
+	Node<T>* head{};
+
+};
---
+++ DataStructures/DataStructures/raiiLList.hpp
@@ -0,0 +1,90 @@
+#pragma once
+#include <iostream>
+#include <memory>
+
+template<typename T>
+struct RNode {
+	T data{};
+	std::shared_ptr<RNode<T>> next{};
+
+};
+
+template<typename T>
+class RAIILinkList {
+
+public:
+	void insertNodeAtBegin(const T& val) {
+		head = std::make_shared<RNode<T>>(val, head);
+	}
+
+	void insertNodeAtEnd(const T& val) {
+		auto newNode = std::make_shared<RNode<T>>(val, nullptr);
+
+		if (!head) {
+			head = std::move(newNode);
+			return;
+		}
+		auto current = head;
+
+		while (current->next) {
+			current = current->next;
+		}
+		current->next = std::move(newNode);
+
+	}
+	bool removeAtBegin() {
+		if (!head) {
+			return false;
+		}
+		head = head->next;
+		return true;
+	}
+
+	bool removeAtEnd() {
+		if (!head) {
+			return false;
+		}
+
+		if (!head->next) return removeAtBegin();
+
+		auto current = head;
+
+		while (current->next->next) {
+			current = current->next;
+		}
+
+		current->next = nullptr;
+		return true;
+	}
+
+	bool removeByValue(const T val) {
+		if (!head) return false;
+
+		if (head->data == val) return removeAtBegin();
+
+		auto current = head;
+
+		while (current->next && current->next->data != val) {
+			current = current->next;
+		}
+
+		if (!current->next) return false;
+
+		current->next = current->next->next;
+		return true;
+	}
+
+
+
+	void print() const {
+		auto current = head;
+
+		while (current) {
+			std::cout << current->data << " -> ";
+			current = current->next;
+		}
+		std::cout << "nullptr\n";
+	}
+private:
+	std::shared_ptr<RNode<T>> head{};
+};
