42bd6bbf2894323895022e29ed48d517485dc5a5
Shearstress021 <shearstress021@gmail.com>
2026-06-16 23:28:48 -0700

    added some functions

--- PhysicsPlay/particleSimulator/main.cpp
+++ PhysicsPlay/particleSimulator/main.cpp
@@ -4,7 +4,11 @@
 #include <random>
 
 
-
+constexpr float MAX_INITIAL_SPEED { 300.0f };
+constexpr float MAX_RADIUS {7.0f};
+constexpr float MIN_RADIUS {3.0f};
+constexpr float DRAG {0.99f};
+constexpr float GRAVITY {9.81f};
 
 struct Particle {
     Vector2 position{};
@@ -26,6 +30,62 @@ class ParticleSimulation{
         ParticleSimulation(){
             for(size_t i{}; i < INITIAL_PARTICLES_AMOUNT; i++){
                 Particle particle{};
+                particle.position = Vector2{random_range(0.0f, GetScreenWidth()), 
+                    random_range(0.0f, GetScreenHeight())};
+                particle.velocity = Vector2{random_range(-MAX_INITIAL_SPEED, MAX_INITIAL_SPEED), 
+                    random_range(-MAX_INITIAL_SPEED, MAX_INITIAL_SPEED)};
+
+                particle.radius = random_range(MIN_RADIUS, MAX_RADIUS);
+
+                m_particles.push_back(particle);
+            }
+        }
+
+
+        void draw(){
+
+            for(const auto& particle: m_particles){
+                DrawCircle(particle.position.x, particle.position.y, particle.radius, WHITE);
+            }
+
+        }
+
+        void update(float deltatime){
+            for(auto& particle : m_particles){
+
+                particle.velocity.x *= DRAG;
+                particle.velocity.y *= DRAG;
+
+                particle.velocity.y += GRAVITY;
+
+                particle.position.x += particle.velocity.x * deltatime;
+                particle.position.y += particle.velocity.y * deltatime;
+
+                bool hitRight { particle.position.x + particle.radius >= GetScreenWidth() };
+                bool hitLeft{ particle.position.x - particle.radius <= 0 };
+                bool hitDown{particle.position.y + particle.radius >= GetScreenHeight()};
+                bool hitUp{ particle.position.y - particle.radius <= 0 };
+
+
+                if(hitRight){
+                    particle.position.x = GetScreenWidth() - particle.radius;
+                    particle.position.x *= -1;
+                }
+
+                if(hitLeft){
+                    particle.position.x =  particle.radius;
+                    particle.position.x *= -1;
+                }
+
+                if(hitUp){
+                    particle.position.y = particle.radius;
+                    particle.position.y *= -1;
+                }
+                if(hitDown){
+                    particle.position.y = GetScreenHeight() - particle.radius;
+                    particle.position.y *= -1;
+                }
+
             }
         }
 
@@ -41,13 +101,15 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Particle Simulator");
 
+    ParticleSimulation simulation{};
     SetTargetFPS(60);
-
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawFPS(10, 10);
+
+        simulation.draw();
+        simulation.update(GetFrameTime());
         EndDrawing();
     }
 
