826cd131ea0fce5c17ec045dd99fd6f682b510cd
Shearstress021 <shearstress021@gmail.com>
2026-06-16 19:23:07 -0700

    unkwown

--- dataStructure/llist.cpp
+++ dataStructure/llist.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
 
+template<typename T>
+struct Node {
+	
+	T data;
+	Node *next{nullptr};
+	explicit Node(const T& val) : data(val) {}
+
+
+
+};
+
+template<typename T>
+class LList{
+
+	private:
+		Node<T>* head{nullptr};
+
+	
+	public:
+
+		void printLL(){
+
+			while(head != nullptr){
+				std::cout << head->data << " --> ";
+				head = head->next;
+			}
+
+		}
+
+		void insertNode(T value){
+
+			Node<T>* num = new Node(value);
+
+			num->next = head;
+			head = num;
+		}
+
+};
 
 
 
 int main(){
 	std::cout << "Hello, World" << '\n';
+
+	LList<int> ll{};
+	ll.insertNode(1);
+	ll.insertNode(2);
+	ll.printLL();
+	std::cout << '\n';
+
 	return 0;
 }
