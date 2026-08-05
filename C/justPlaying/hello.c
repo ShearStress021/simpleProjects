93aa605217a6bbc11b4c06b04e2845979eb94e6a
Shearstress021 <shearstress021@gmail.com>
2026-08-05 13:50:53 -0700

    unknown changes

--- C/justPlaying/hello.c
+++ C/justPlaying/hello.c
@@ -7,11 +7,19 @@ void my_strCat(char *buffer, char *a, char *b)
 	sprintf(buffer, "%s%s", a, b);
 }
 int main(){
+
+	for(int i =0, j = 10; j; j--,i++){
+		printf("j = %d\n", j);
+		printf("i= %d\n", i);
+
+	}
+	printf("\n");
 	char result[5]={0};
 	my_strCat(result, "hi", "ya");
 	printf("\033[0;31mFAIL: %s\n\033[0m", "Hey");
 	printf("result : %s\n", result);
 }
+
 
 
 
