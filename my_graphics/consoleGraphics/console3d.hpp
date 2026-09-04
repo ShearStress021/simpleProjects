66370e65af5f32db4eeee33dbcf44a7981206d79
shearstress021 <shearstress021@gmail.com>
2026-09-03 21:50:59 -0700

    drawline

--- my_graphics/consoleGraphics/console3d.hpp
+++ my_graphics/consoleGraphics/console3d.hpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <thread>
 #include <vector>
+#include <math.h>
 
 struct Vec3 {
 	float x,y, z;
@@ -61,14 +62,11 @@ class Console3d {
 
 			}
 
-
 			CONSOLE_SCREEN_BUFFER_INFO screenInfo{};
 			if(!GetConsoleScreenBufferInfo(handleConsole, &screenInfo)){
 				std::cerr << "GetConsoleScreenBufferInfo Failed\n";
 				CloseHandle(handleConsole);
 				return 1;
-
-
 			}
 
 			windowRect = {0,0, (short)(screenWidth - 1), (short)(screenHeight -1 )};
@@ -87,6 +85,124 @@ class Console3d {
 
 		}
 
+		void draw(int x, int y, short c = 0x8000, short color = 0x000F){
+			if(x>= 0 && x < screenWidth && y >= 0 && y < screenHeight){
+				screen[y * screenWidth + x].Char.UnicodeChar = c;
+				screen[y * screenWidth + x].Attributes = color;
+
+			}
+
+		}
+
+		void drawLinee(int x1, int y1, int x2, int y2, short c = '/', short color = 0x0009){
+			int dx = x2 - x1;
+			int dy = y2 - y1;
+
+			float length = std::sqrt(dx * dx + dy * dy);
+			float xstep = dx / length;
+			float ystep = dy / length;
+
+			for(float i = 0; i <= length;i += 1.0f){
+				int x = static_cast<int>(x1 + xstep * i);
+				int y = static_cast<int>(y1 + ystep * i);
+
+				draw(x, y, 'o', color);
+			}
+
+		}
+		void fillColor(int x1, int y1, int x2,int y2, short c = '-', short color = 0x000F){
+			clip(x1, y1);
+			clip(x2,y2);
+
+			for(int x = x1; x < x2; x++){
+				for(int y = y1; y < y2; y++)
+					draw(x,y,c,color);
+			}
+		}
+
+		void clip(int &x, int &y){
+			if(x < 0) x = 0;
+			if(x >= screenWidth) x = screenWidth;
+			if(y < 0) y = 0;
+			if(y >= screenHeight) y = screenHeight;
+
+		}
+		void drawLine(int x1, int y1, int x2, int y2, short c = 0x2588, short color = 0x000F){
+			int dx = std::abs(x2 - x1);
+			int dy = std::abs(y2 - y1);
+
+			int sx = (x1 < x2) ? 1 : -1;
+			int sy = (y1 < y2) ? 1 : -1;
+
+			int error = dx - dy;
+
+			while(true){
+				draw(x1, y1, c, color);
+				
+				if(x1 == x2 && y1 == y2) break;
+				
+				int e2 = 2 * error;
+
+				if(e2 > -dy){
+					error -= dy;
+					x1 += sx;
+				}
+				if(e2 < dx){
+					error += dx;
+					y1 += sy;
+				}
+			}
+		}
+
+		void drawline(int x1, int y1, int x2, int y2, short c=0x2588, short color=0x000F){
+			int x, y, dx, dy, dx1, dy1, px, py, xe, ye,i;
+			dx = x2 - x1; dy= y2 - y1;
+			dx1 = abs(dx); dy1 = abs(dy);
+			px = 2 * dy1 - dx1; py = 2 * dx1 - dy1;
+			if(dy1 <= dx1){
+				if(dx >= 0){
+					x = x1; y = y1; xe = x2;
+				}else {
+					x = x2; y = y2; xe = x1;
+				}
+				draw(x, y,c, color);
+
+				for(i = 0; x < xe; i++){
+					x += 1;
+					if(px < 0) px = px + 2 * dy1;
+					else {
+						if((dx < 0 && dy < 0 ) || (dx > 0 && dy > 0)) y+=1;
+						else y -= 1;
+						px = px + 2 * (dy1 - dx1);
+					}
+					draw(x, y, c, color);
+				}
+
+			} else {
+				if(dy >= 0){
+					x = x1; y = y1; ye = y2;
+				}else {
+					x = x2; y = y2; ye = y1;
+				}
+				draw(x,y,c,color);
+
+				for(i = 0; y < ye; i++){
+					y += 1;
+					if(py <= 0)
+						py = py + 2 * dy1;
+					else {
+						if((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) x+=1; 
+						else x -= 1;
+						py = py + 2 * (dx1 - dy1);
+
+					}
+					draw(x, y, c, color);
+				}
+			}
+		}
+
+
+
 		void init(){
 			renderer();
 
@@ -102,15 +218,21 @@ class Console3d {
 
 			while(!running){
 				// screen clearing  
-				for(int i{}; i < screenWidth * screenHeight; i++) {
-					screen[i].Char.UnicodeChar= ' ';
-					screen[i].Attributes = FOREGROUND_GREEN;
-				}
+				//
+				fillColor(0,0,screenWidth, screenHeight, ' ',0x000F);
+				//for(int i{}; i < screenWidth * screenHeight; i++) {
+				//	screen[i].Char.UnicodeChar= ' ';
+				//	screen[i].Attributes = FOREGROUND_GREEN;
+				//}
 				// boarder 
 				for(int i {}; i < screenWidth; i++){
 					screen[i].Char.UnicodeChar = '=';
 					screen[2* screenWidth + i].Char.UnicodeChar= '=';
 				}
+
+				drawLine(10,20,50,35);
+				drawline(20,20,60,35);
+				drawLinee(30,20,70,35);
 
 			
 				WriteConsoleOutput(handleConsole, screen, 
